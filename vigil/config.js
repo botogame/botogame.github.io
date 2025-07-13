3144a4cab524863db8221c0f9279428be3b828d8
Андрей Волков <dominic.of.Russia@gmail.com>
2025-07-14 08:32:15 +1000

    Update config.js

--- vigil/config.js
+++ vigil/config.js
@@ -26,16 +26,6 @@ let days2 = [
     'Субботняя'
 ];
 
-var meditation_days = [
-        { id: 0, name: 'воскресенье', planet: 'Солнце', file: 'https://moosic.my.mail.ru/file/07971b4232f6603ac3f9f4cea8b5d281.mp3', image: 'sun.png' },
-        { id: 1, name: 'понедельник', planet: 'Венера', file: 'https://moosic.my.mail.ru/file/410e5b3370028b3d87397470d2acf392.mp3', image: 'venus.png' },
-        { id: 2, name: 'вторник', planet: 'Марс', file: 'https://moosic.my.mail.ru/file/a1e42b4063feb96097296e6b4e26c835.mp3', image: 'mars.png' },
-        { id: 3, name: 'среда', planet: 'Луна', file: 'https://moosic.my.mail.ru/file/7c8d2c607b56fe2565f81be52adb41ff.mp3', image: 'moon.png' },
-        { id: 4, name: 'четверг', planet: 'Сатурн', file: 'https://moosic.my.mail.ru/file/5ef26686a6f1643f882dab3f80e30888.mp3', image: 'saturn.png' },
-        { id: 5, name: 'пятница', planet: 'Юпитер', file: 'https://moosic.my.mail.ru/file/b1ef3754e4104dd8014e66df88b018ff.mp3', image: 'jupiter.png' },
-        { id: 6, name: 'суббота', planet: 'Меркурий', file: 'https://moosic.my.mail.ru/file/f5b35c89e7affcf466d0bbda30bf9ce7.mp3', image: 'mercury.png' }
-    ];
-
 // Масти для мужского и женского пола
 let ar_replace_mast_male = {
     '': '',
